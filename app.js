aa8a9288e118609378d54269149e595ab3676965
Kingson <kingsonejikeme@gmail.com>
2021-10-31 17:50:27 +0100

    Added Procfile and configured dynamic listening PORT

--- app.js
+++ app.js
@@ -103,7 +103,11 @@ app.get("/posts/:postId", function(req, res) {
 
 
 //START THE SERVER AT PORT 3000
+let port = process.env.PORT;
+if(port== null || port==""){
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
